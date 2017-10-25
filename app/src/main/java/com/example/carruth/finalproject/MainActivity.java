878c4caa3ad0bd8cbfbb561ab2f0e79d0b957908
htatemitchell <mit15007@byui.edu>
2017-10-25 14:39:20 -0600

    Made a lame print line function at line 14 to 17

--- app/src/main/java/com/example/carruth/finalproject/MainActivity.java
+++ app/src/main/java/com/example/carruth/finalproject/MainActivity.java
@@ -10,4 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Tate Made this comment and this lame function
+    protected void nonsense() {
+        System.out.println("YEEHAWH");
+    }
+
 }
