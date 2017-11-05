e7255b760ec4809b8d3ed92cba662298d9875bda
Daniel Thomsen <danthomsen42@gmail.com>
2017-11-05 00:04:18 -0600

    Working on comparison functions - not finished yet

--- app/src/main/java/com/example/carruth/finalproject/CreateAccountActivity.java
+++ app/src/main/java/com/example/carruth/finalproject/CreateAccountActivity.java
@@ -2,13 +2,15 @@ package com.example.carruth.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import java.util.Map;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
     //Private User user;
-
+EditText p1 = (EditText)findViewById(R.id.password);
+EditText p2 = (EditText)findViewById(R.id.password_confirm);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,17 @@ public class CreateAccountActivity extends AppCompatActivity {
     public Boolean checkPass(String p, String pp){
         return false;
     }
+    // NOT FINISHED YET
+    public Boolean comparePass(EditText p1, EditText p2){
+        if(p1.getText().toString().equals( p2.getText().toString())){
+         return true;
+        }
+        else {
+            return false;
+        }
+    }
+//
+
 
     public Boolean everythingFilled(String fName, String lName, String address, String city,
                                     String zip, String email, String password, String password2){
