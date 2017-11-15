32e16060d97c2ac16f3e8997ca341bc10ba49612
Daniel Thomsen <danthomsen42@gmail.com>
2017-11-15 14:34:25 -0700

    Increased functionality of the calendar and time select

--- app/src/main/java/com/example/carruth/finalproject/CalanderActivity.java
+++ app/src/main/java/com/example/carruth/finalproject/CalanderActivity.java
@@ -2,20 +2,42 @@ package com.example.carruth.finalproject;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CalendarView;
 
 public class CalanderActivity extends AppCompatActivity {
 
+    private  static final String TAG = "CalendarActivity";
+    private CalendarView mCalendarView;
+    private Bundle bund;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calander);
-    }
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
+            public Bundle bundle;
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+                String date = month + "/" + dayOfMonth + "/"+ year ;
+                Log.d(TAG, "onSelectedDayChange: mm/dd/yyyy:" + date);
+                Intent intent = new Intent(CalanderActivity.this,ChooseTimeActivity.class);
+                bundle = new Bundle();
+                bundle.putString("date",date);
+                bund = bundle;
+            }
+
+        });
+
+    }
     public void onChooseDate(View view){
-        Intent intent = new Intent(getApplicationContext(),ChooseTimeActivity.class);
+        Intent intent = new Intent(getApplicationContext(), ChooseTimeActivity.class);
+        intent.putExtras(bund);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/carruth/finalproject/ChooseTimeActivity.java
+++ app/src/main/java/com/example/carruth/finalproject/ChooseTimeActivity.java
@@ -3,7 +3,9 @@ package com.example.carruth.finalproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import java.io.LineNumberReader;
 
@@ -13,6 +15,12 @@ public class ChooseTimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_time);
+        Bundle bund = getIntent().getExtras();
+        String dated = bund.getString("date");
+        String TAG = "Date";
+        TextView textView = (TextView) findViewById(R.id.timeDate);
+        textView.setText(dated);
+        Log.d(TAG, "1234");
     }
 
     public void onSelectTime(View view){
