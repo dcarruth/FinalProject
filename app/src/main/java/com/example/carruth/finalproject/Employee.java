17cd856bb456564ef83b5808e226c9a24e38f72a
Daniel Thomsen <danthomsen42@gmail.com>
2017-11-25 19:35:25 -0700

    Employee class... not done, but a good start at least

---
+++ app/src/main/java/com/example/carruth/finalproject/Employee.java
@@ -0,0 +1,88 @@
+package com.example.carruth.finalproject;
+
+
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.HashMap;
+
+import java.util.List;
+import java.util.Map;
+/**
+ * Created by Daniel Thomsen on 11/20/2017.
+ */
+
+public class Employee {
+
+    private Map<String, String> employeeInfo;
+    private List<String> availableTimes;
+    private List<String> availableTasks;
+    private List<String> availableAreas;
+    private Map<String, String> taskLength;
+    private DatabaseReference theDatabase;
+    private Employee employee;
+    Employee(Map<String, String> em) {
+        employeeInfo = em;
+        availableTimes = null;
+        availableAreas = null;
+        availableTasks = null;
+    }
+
+    //Just the Getters
+
+    public List<String> getAvailableTasks() {
+        return availableTasks;
+    }
+
+    public Map<String, String> getEmployeeInfo() {
+        return employeeInfo;
+    }
+
+    public List<String> getAvailableTimes() {
+        return availableTimes;
+    }
+
+    public List<String> getAvailableAreas() {
+        return availableAreas;
+    }
+
+
+    //Just the Setters
+
+    public void setEmployeeInfo(Map<String, String> employeeInfo) {
+        this.employeeInfo = employeeInfo;
+    }
+
+    public void setAvailableTasks(List<String> availableTasks) {
+        this.availableTasks = availableTasks;
+    }
+
+    public void setAvailableTimes(List<String> availableTimes) {
+        this.availableTimes = availableTimes;
+    }
+
+    public void setAvailableAreas(List<String> availableAreas) {
+        this.availableAreas = availableAreas;
+    }
+
+    public void saveData(String save){
+        FirebaseDatabase data = FirebaseDatabase.getInstance();
+
+
+
+    }
+
+    public Employee retrieveData(){
+
+
+
+
+        return employee;
+    }
+
+
+}
